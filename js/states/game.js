d9f31688205b8167da5f52a0bc02813f2256c77b
jefree <jeffersongarzonm@gmail.com>
2016-01-27 22:49:18 -0500

    add image, load assets, create ship and move ship

--- js/states/game.js
+++ js/states/game.js
@@ -1,22 +1,43 @@
 var MyFirstGame = MyFirstGame || {};
 
-MyFirstGame.GameState = function(game){
+MyFirstGame.GameState = function(){
   //initialize global features for your game
+  this.asteroids = [];
+  this.score = 0;
+
 }
 
 MyFirstGame.GameState.prototype.preload = function() {
-  // load al the assets you need
+  // load all the assets you need
+  this.load.image('ship', 'img/ship.png');
+  this.load.spritesheet('asteroid', 'img/asteroid.png', 120, 120);
 }
 
 MyFirstGame.GameState.prototype.create = function() {
   // create the entities for your game
+  this.ship = this.game.add.sprite(0, 0, 'ship');
+
+  this.ship.anchor.set(0.5, 1);
+
+  this.ship.x = this.game.world.centerX;
+  this.ship.y = this.game.world.height;
+
+  //config the keyboard input
+  this.left = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+  this.right = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
 }
 
 MyFirstGame.GameState.prototype.update = function() {
   // check the rules of your game
-  // jump between states
-  // this.game.state.start('another state');
-  console.log("playing");
+
+  if (this.left.isDown) {
+    this.ship.x -= 7;
+  }
+
+  if (this.right.isDown) {
+    this.ship.x += 7;
+  }
 }
 
 MyFirstGame.GameState.prototype.render = function() {
